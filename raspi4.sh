e8bdf23b0cca64e8776cc8dca78c6766674d227f
jediking2000 <50193365+jediking2000@users.noreply.github.com>
2019-07-01 09:35:20 -0600

    Update raspi4.sh
    Added shit

--- raspi4.sh
+++ raspi4.sh
@@ -1,5 +1,36 @@
 #!/bin/sh
 
-sudo apt update
+sudo apt update 
+sudo apt upgrade -y
+echo "updated system"
 
-sudo apt upgrade
+sudo apt-get install perl libnet-ssleay-perl openssl libauthen-pam-perl libpam-runtime libio-pty-perl apt-show-versions python -y
+eecho "installed webmin prerequisites"
+
+wget http://prdownloads.sourceforge.net/webadmin/webmin_1.910_all.deb
+
+sudo dpkg --install webmin_1.910_all.deb
+
+echo "webmin (should be) installed"
+
+
+sudo apt-get install apt-transport-https ca-certificates software-properties-common -y
+
+sudo curl -fsSL get.docker.com -o get-docker.sh && sh get-docker.sh
+sudo usermod -aG docker pi
+
+sudo curl https://download.docker.com/linux/raspbian/gpg
+
+echo "deb https://download.docker.com/linux/raspbian/ stretch stable" >> /etc/apt/sources.list
+sudo apt update 
+sudo apt upgrade -y
+
+
+systemctl start docker.service
+
+docker info > ~/dockerinfo
+
+echo "installed Docker"
+
+sudo apt install docker-compose
+echo "installed Docker compose"
