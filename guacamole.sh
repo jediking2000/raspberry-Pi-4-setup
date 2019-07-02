50c90c054cdc46f2503d7e2f229a4c03a1a78793
jediking2000 <50193365+jediking2000@users.noreply.github.com>
2019-07-02 09:53:25 -0600

    Create guacamole.sh

---
+++ guacamole.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+sudo apt update
+sudo apt upgrade -y
+
+sudo apt-get install libcairo2-dev -y
+sudo apt-get install libjpeg62-turbo-dev -y
+sudo apt-get install libpng12-dev -y
+sudo apt-get install libossp-uuid-dev -y
+
+sudo apt-get install libavcodec-dev libavutil-dev libswscale-dev -y 
+sudo apt-get install libpango1.0-dev -y
+sudo apt-get install libssh2-1-dev -y
+sudo apt-get install libtelnet-dev -y
+sudo apt-get install libvncserver-dev -y
+sudo apt-get install libpulse-dev -y
+sudo apt-get install libssl-dev -y
+sudo apt-get install libvorbis-dev -y
+sudo apt-get install libwebp-dev -y
+
