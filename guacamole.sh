206e81590364e90ebe83ea9ac163cee4791a6f31
jediking2000 <50193365+jediking2000@users.noreply.github.com>
2019-07-04 08:59:21 -0600

    Update guacamole.sh

--- guacamole.sh
+++ guacamole.sh
@@ -3,28 +3,15 @@
 sudo apt update
 sudo apt upgrade -y
 
-sudo apt-get install libcairo2-dev -y
-sudo apt-get install libjpeg62-turbo-dev -y
-sudo apt-get install libpng12-dev -y
-sudo apt-get install libossp-uuid-dev -y
-
-sudo apt-get install libavcodec-dev libavutil-dev libswscale-dev -y 
-sudo apt-get install libpango1.0-dev -y
-sudo apt-get install libssh2-1-dev -y
-sudo apt-get install libtelnet-dev -y
-sudo apt-get install libvncserver-dev -y
-sudo apt-get install libpulse-dev -y
-sudo apt-get install libssl-dev -y
-sudo apt-get install libvorbis-dev -y
-sudo apt-get install libwebp-dev -y
 
 sudo apt-get install -y xrdp
 
 
 
-
-
 sudo apt-get install -y libcairo2-dev libjpeg62-turbo-dev libpng12-dev libossp-uuid-dev libvncserver-dev freerdp-x11 libssh-dev libssh2-1-dev libfreerdp-dev libtelnet-dev
+
+sudo apt-get install -y libavcodec-dev libavutil-dev libswscale-dev libpango1.0-dev libpulse-dev libssl-dev libvorbis-dev libwebp-dev
+
 
 sudo apt-get update && sudo apt-get install oracle-java8-jdk
 export JAVA_HOME=/usr/lib/jvm/jdk-8-oracle-arm32-vfp-hflt
